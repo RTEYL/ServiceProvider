cc64ce636bf3ffce7e955a7b170285cb000ea851
RTEYL <tyler-w-jones@hotmail.com>
2020-07-22 14:16:49 -0700

    finalize service#create

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -7,16 +7,26 @@ class ServicesController < ApplicationController
 
   def new
     redirect_if_not_provider
+    @provider = Provider.find_by_id(params[:provider_id])
     @service = Service.new
   end
 
   def create
     @service = Service.new(service_params)
+    if @service.save
+      redirect_to provider_service_path(params[:provider_id], @service)
+    else
+      render :new
+    end
   end
 
   private
 
   def service_params
-    params.require(:service).permit(:type, :description, :provider_id)
+    if params[:provider_id] == params[:service][:provider_id]
+      params.require(:service).permit(:service_type, :description, :provider_id)
+    else
+      redirect_to new_provider_service_path(params[:provider_id])
+    end
   end
 end
