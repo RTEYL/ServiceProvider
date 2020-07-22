d92e5cc482302dff10cdfeaa9cbaf0de9d89f2ee
RTEYL <tyler-w-jones@hotmail.com>
2020-07-21 17:26:41 -0700

    create user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    @user = User.new(user_params)
+    @user = Provider.new(user_params.except(:service_provider)) if @user.service_provider
+    binding.pry
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :service_provider)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+
   resources :sessions
   resources :services
-  resources :users
+  resources :users, only: [:show]
   resources :providers
   resources :service_requests
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
