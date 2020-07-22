71e06379593b81b824d8f7972d8babb38ffb71d0
RTEYL <tyler-w-jones@hotmail.com>
2020-07-22 11:59:45 -0700

    init service controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,3 +1,21 @@
 class ServicesController < ApplicationController
   before_action :redirect_if_not_logged_in
+
+  def index
+    @services = Service.all
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:type, :description, :provider_id)
+  end
 end
