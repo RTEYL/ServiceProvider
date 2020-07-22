c8b234e60eca329a062b8e86eefdf57f96ae293c
RTEYL <tyler-w-jones@hotmail.com>
2020-07-21 19:07:48 -0700

    set up login with sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,30 @@
 class SessionsController < ApplicationController
 
   def create
-
+    user = user_or_provider
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
     session.delete :user_id
     redirect_to root_path
+  end
+
+  private
+
+  def user_or_provider
+    if  user = User.find_by(email: params[:user][:email])
+      user
+    elsif provider = Provider.find_by(email: params[:user][:email])
+      provider
+    else
+      nil
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
   delete 'logout', to: 'sessions#destroy'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 
   resources :sessions
   resources :services
