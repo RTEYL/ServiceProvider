c2eb1aac41a843aed5a0891b20397932ce74a4db
RTEYL <tyler-w-jones@hotmail.com>
2020-07-22 13:31:22 -0700

    fix current_user helper to accept provider

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) || Provider.find_by_id(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
   def redirect_if_not_provider
-    false if self.method_defined? :service_provider
+    false if respond_to?(:service_provider)
   end
 
 end
