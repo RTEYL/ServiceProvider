257f3a71f1604e8cf42642bd3268b5f74ff836c9
RTEYL <tyler-w-jones@hotmail.com>
2020-07-22 15:43:36 -0700

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,5 @@ Rails.application.routes.draw do
     resources :services, except: [:index]
     resources :service_requests, only: [:index, :edit]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
-# providers/id/services/id
-# providers/id/services/new
-# users/id/service_requests
-# users/id/service_requests/new
-# providers/id/service_requests
