9ad3adb6319bb6db3aab2389af8d757cac1058c7
RTEYL <tyler-w-jones@hotmail.com>
2020-07-22 19:55:56 -0700

    init service request

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -1,18 +1,26 @@
 class ServiceRequestsController < ApplicationController
   before_action :redirect_if_not_logged_in
 
+  def index
+    @provider = Provider.find_by_id(current_user)
+  end
+
   def create
     @service_request = ServiceRequest.new(service_request_params)
     if @service_request.save
-      redirect_to user_service_requests_path(current_user)
+      redirect_to user_path(current_user)
     else
       redirect_back fallback_location: root_path
     end
   end
 
+  def update
+    binding.pry
+  end
+
   private
 
   def service_request_params
-    params.require(:service_request).permit(:provider_id, :user_id)
+    params.require(:service_request).permit(:service_id, :provider_id, :user_id)
   end
 end
