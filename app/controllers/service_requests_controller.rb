f9080c8c6be711729a67204fc4728ec6c18eb0e8
RTEYL <tyler-w-jones@hotmail.com>
2020-07-23 13:09:29 -0700

    update/destroy service request globally

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -15,12 +15,23 @@ class ServiceRequestsController < ApplicationController
   end
 
   def update
-    binding.pry
+    @service_request = ServiceRequest.find_by_id(params[:id])
+    if @service_request
+      @service_request.update(service_request_params)
+      destroy if @service_request.completed
+    else
+      redirect_back fallback_location: root_path
+    end
+    redirect_to provider_service_requests_path(current_user)
+  end
+
+  def destroy
+    ServiceRequest.delete(params[:id])
   end
 
   private
 
   def service_request_params
-    params.require(:service_request).permit(:service_id, :provider_id, :user_id)
+    params.require(:service_request).permit(:completed, :service_id, :provider_id, :user_id)
   end
 end
