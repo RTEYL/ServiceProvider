cc6959f36d5065d7ac4a08753cbec03bbf28fc5b
RTEYL <tyler-w-jones@hotmail.com>
2020-07-22 18:27:08 -0700

    create service request

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -1,3 +1,18 @@
 class ServiceRequestsController < ApplicationController
   before_action :redirect_if_not_logged_in
+
+  def create
+    @service_request = ServiceRequest.new(service_request_params)
+    if @service_request.save
+      redirect_to user_service_requests_path(current_user)
+    else
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  private
+
+  def service_request_params
+    params.require(:service_request).permit(:provider_id, :user_id)
+  end
 end
