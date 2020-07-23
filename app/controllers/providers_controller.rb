527791acbd878b47845300412557097f402d1f8a
RTEYL <tyler-w-jones@hotmail.com>
2020-07-22 18:26:20 -0700

    add before action to restrict user type

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,5 +1,6 @@
 class ProvidersController < ApplicationController
  before_action :redirect_if_not_logged_in
+ before_action :redirect_if_not_provider
   def show
     @provider = Provider.find_by_id(params[:id])
     @service = @provider.service
