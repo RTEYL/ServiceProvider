14e58c4b29efca189893e694a90409f8df2a4cd8
RTEYL <tyler-w-jones@hotmail.com>
2020-07-26 15:05:04 -0700

    fix missed omniauth commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
+  resources :reviews
   resources :services, only: [:index]
   resources :users, except: [:index] do
     resources :services, except: [:index]
