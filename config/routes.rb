b06ca001f9cb7f645da6c2006f9e49dedb96ffc9
RTEYL <tyler-w-jones@hotmail.com>
2020-07-26 19:57:18 -0700

    update routes and links for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   end
   resources :users, except: [:index] do
     resources :service_requests
-    resources :services, except: [:show, :index] do
-      resources :reviews
+    resources :services, except: [:show, :index], shallow:true do
+      resources :reviews, only: [:new, :create, :edit]
     end
   end
 
