8848f6c11547d6c513abf202b6cb18654a49d487
RTEYL <tyler-w-jones@hotmail.com>
2020-07-28 16:34:51 -0700

    setup user model with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
   has_many :service_requests
   has_many :reviews
 	has_many :services, through: :reviews
@@ -10,6 +14,13 @@ class User < ApplicationRecord
 
   def fullname
     "#{first_name}" + ' ' + "#{last_name}"
+  end
+
+  def self.create_from_provider_data(provider_data)
+    wher(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
   end
 
 end
