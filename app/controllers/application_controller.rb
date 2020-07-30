1d0ddc19518d3a19524b8428ab468148cfba96ac
RTEYL <tyler-w-jones@hotmail.com>
2020-07-29 20:20:26 -0700

    increase use of devise accross the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user, :logged_in?
 
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!session[:user_id]
+  def after_sign_in_path_for(resource)
+   user_path(current_user.id)
   end
 
   def redirect_if_not_logged_in
-    redirect_to root_path if !logged_in?
+    redirect_to root_path if !user_signed_in?
   end
 
   def redirect_if_not_provider
--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,10 +1,10 @@
-class OmniauthController < ApplicationController
+class OmniauthController < Devise::SessionsController
 
   def github
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
-      sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      sign_in_and_redirect @user
     else
       failure
     end
