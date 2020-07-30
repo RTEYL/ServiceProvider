c71387e551353f2df48db8a779fc22d9c3a0b109
RTEYL <tyler-w-jones@hotmail.com>
2020-07-29 20:20:50 -0700

    reconfig devise/omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,17 @@ class User < ApplicationRecord
   has_many :reviews
 	has_many :services, through: :reviews
   has_one :service
-  has_secure_password
-  validates :first_name, :last_name, presence: true
-  validates :email, uniqueness: true, presence: true
-
+  validates :first_name, :last_name, presence: true, if: -> {provider.blank?}
+  validates :email, uniqueness: true, presence: true, allow_blank: true, if: -> {provider.blank?}
 
   def fullname
     "#{first_name}" + ' ' + "#{last_name}"
   end
 
   def self.create_from_provider_data(provider_data)
-    wher(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
-      user.email = provider_data.info.email
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.email = provider_data.info.email if !nil
+      user.first_name = provider_data.info.nickname
       user.password = Devise.friendly_token[0, 20]
     end
   end
